1c85c59ca6172e9115601d5d59923bf123ca0ae5
Megan Warnock <megandwarnock@gmail.com>
2017-04-01 18:04:18 -0700

    add delete route for stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -92,6 +92,16 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    //deltet stylist
+    post("/stylists/:stylist_id/delete", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params("stylist_id")));
+      stylist.delete();
+      String url = String.format("/stylists");
+      response.redirect(url);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
 
   }
